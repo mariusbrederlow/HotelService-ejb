2b8ade5df7c7a8b93f6877a1fc671d8efae75d7f
mariusbrederlow <m.brederlow@ostfalia.de>
2013-02-01 18:43:09 +0100

    Init

---
+++ src/java/SessionBeans/LocationService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SessionBeans;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author mariusbrederlow
+ */
+@Stateless
+@LocalBean
+public class LocationService {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
